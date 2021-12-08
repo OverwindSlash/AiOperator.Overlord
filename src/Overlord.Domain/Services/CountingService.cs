3dcfe22a9953be83059f9bd4a89b884227a6f0cc
OverwindSlash <overwind@outlook.com>
2021-12-08 21:58:55 +0800

    Refactor CountingService

--- src/Overlord.Domain/Services/CountingService.cs
+++ src/Overlord.Domain/Services/CountingService.cs
@@ -9,14 +9,14 @@ using System.Threading.Tasks;
 
 namespace Overlord.Domain.Services
 {
-    public class CountingService : IObserver<ObjectExpiredEvent>
+    public class CountingService : ObserverBase<ObjectExpiredEvent>
     {
         private RoadDefinition _roadDefinition;
         private List<Tuple<EnterLine, LeaveLine>> _countLines;
         private bool _isDoubleLineCounting;
 
-        private List<EnterLine> _enterLines;
-        private List<LeaveLine> _leaveLines;
+        private readonly List<EnterLine> _enterLines;
+        private readonly List<LeaveLine> _leaveLines;
 
         // objectId -> count line index
         private readonly ConcurrentDictionary<string, int> _objsCrossedEnterLine;
@@ -25,8 +25,9 @@ namespace Overlord.Domain.Services
         // to avoid repeat count, objectId -> frameId
         private readonly ConcurrentDictionary<string, long> _objsCounted;
 
-        // landId -> (objectType -> count)
-        private readonly ConcurrentDictionary<int, ConcurrentDictionary<string, int>> _laneObjectTypeCounts;
+        // lane type counting, landId -> (objectType -> count)
+        private readonly ConcurrentDictionary<int, ConcurrentDictionary<string, int>> _laneObjectTypeCount;
+
 
         public ConcurrentDictionary<string, int> ObjsCrossedEnterLine => _objsCrossedEnterLine;
 
@@ -34,31 +35,34 @@ namespace Overlord.Domain.Services
 
         public ConcurrentDictionary<string, long> ObjsCounted => _objsCounted;
 
+
         public CountingService()
         {
+            _enterLines = new List<EnterLine>();
+            _leaveLines = new List<LeaveLine>();
+
             _objsCrossedEnterLine = new ConcurrentDictionary<string, int>();
             _objsCrossedLeaveLine = new ConcurrentDictionary<string, int>();
             _objsCounted = new ConcurrentDictionary<string, long>();
-            _laneObjectTypeCounts = new ConcurrentDictionary<int, ConcurrentDictionary<string, int>>();
-            _enterLines = new List<EnterLine>();
-            _leaveLines = new List<LeaveLine>();
+
+            _laneObjectTypeCount = new ConcurrentDictionary<int, ConcurrentDictionary<string, int>>();
         }
 
         public void SetRoadDefinition(RoadDefinition roadDefinition)
         {
             _roadDefinition = roadDefinition;
-            _isDoubleLineCounting = roadDefinition.IsDoubleLineCounting;
             _countLines = _roadDefinition.CountLines;
+            _isDoubleLineCounting = roadDefinition.IsDoubleLineCounting;
 
             _objsCrossedEnterLine.Clear();
             _objsCrossedLeaveLine.Clear();
             _objsCounted.Clear();
 
-            foreach (ConcurrentDictionary<string, int> typeCount in _laneObjectTypeCounts.Values)
+            foreach (ConcurrentDictionary<string, int> typeCount in _laneObjectTypeCount.Values)
             {
                 typeCount.Clear();
             }
-            _laneObjectTypeCounts.Clear();
+            _laneObjectTypeCount.Clear();
 
             _enterLines.Clear();
             _leaveLines.Clear();
@@ -72,12 +76,14 @@ namespace Overlord.Domain.Services
 
         public void CountTrafficeObject(TrafficObjectInfo toi)
         {
+            // avoid repeat counting
             if (_objsCounted.ContainsKey(toi.Id))
             {
                 toi.WasCounted = true;
                 return;
             }
 
+            // no movement, skip counting
             if ((toi.MotionInfo.XOffset == 0) && (toi.MotionInfo.YOffset == 0))
             {
                 return;
@@ -89,6 +95,22 @@ namespace Overlord.Domain.Services
             NormalizedPoint lastPos = new NormalizedPoint(_roadDefinition.ImageWidth, _roadDefinition.ImageHeight,
                 toi.CenterX - toi.MotionInfo.XOffset, toi.CenterY - toi.MotionInfo.YOffset);
 
+            CheckCrossedEnterLines(toi, currentPos, lastPos);
+
+            CheckCrossedLeaveLines(toi, currentPos, lastPos);
+            
+            bool needCounting = WillNeedCounting(toi);
+
+            if (needCounting)
+            {
+                UpdateLaneObjectTypeCount(toi);
+
+                UpdateObjectCounted(toi);
+            }
+        }
+
+        private void CheckCrossedEnterLines(TrafficObjectInfo toi, NormalizedPoint currentPos, NormalizedPoint lastPos)
+        {
             if (!_objsCrossedEnterLine.ContainsKey(toi.Id))
             {
                 foreach (EnterLine enterLine in _enterLines)
@@ -99,7 +121,10 @@ namespace Overlord.Domain.Services
                     }
                 }
             }
+        }
 
+        private void CheckCrossedLeaveLines(TrafficObjectInfo toi, NormalizedPoint currentPos, NormalizedPoint lastPos)
+        {
             if (!_objsCrossedLeaveLine.ContainsKey(toi.Id))
             {
                 foreach (LeaveLine leaveLine in _leaveLines)
@@ -110,66 +135,62 @@ namespace Overlord.Domain.Services
                     }
                 }
             }
-            
-            if (!_objsCrossedLeaveLine.ContainsKey(toi.Id))
-            {
-                foreach (LeaveLine leaveLine in _leaveLines)
-                {
-                    if (leaveLine.IsCrossedLine(currentPos, lastPos))
-                    {
-                        _objsCrossedLeaveLine.TryAdd(toi.Id, _leaveLines.IndexOf(leaveLine));
-                    }
-                }
-            }
+        }
 
-            bool needCounting = false;
+        private bool WillNeedCounting(TrafficObjectInfo toi)
+        {
             if (_isDoubleLineCounting)
             {
                 if (_objsCrossedEnterLine.ContainsKey(toi.Id) && _objsCrossedLeaveLine.ContainsKey(toi.Id))
                 {
-                    needCounting = true;
+                    return true;
                 }
             }
             else
             {
                 if (_objsCrossedEnterLine.ContainsKey(toi.Id))
                 {
-                    needCounting = true;
+                    return true;
                 }
             }
 
-            if (needCounting)
+            return false;
+        }
+
+        private void UpdateObjectCounted(TrafficObjectInfo toi)
+        {
+            if (!_objsCounted.ContainsKey(toi.Id))
             {
-                if (!_laneObjectTypeCounts.ContainsKey(toi.LaneIndex))
-                {
-                    _laneObjectTypeCounts.TryAdd(toi.LaneIndex, new ConcurrentDictionary<string, int>());
-                }
-
-                _laneObjectTypeCounts.TryGetValue(toi.LaneIndex, out var typesCountsByLaneId);
-                if (!typesCountsByLaneId.ContainsKey(toi.Type))
-                {
-                    typesCountsByLaneId.TryAdd(toi.Type, 0);
-                }
-
-                typesCountsByLaneId.TryGetValue(toi.Type, out var typeCounts);
-                typeCounts++;
-                typesCountsByLaneId.TryUpdate(toi.Type, typeCounts, typeCounts - 1);
-
-                if (!_objsCounted.ContainsKey(toi.Id))
-                {
-                    _objsCounted.TryAdd(toi.Id, toi.FrameId);
-                }
+                _objsCounted.TryAdd(toi.Id, toi.FrameId);
             }
+        }
+
+        private void UpdateLaneObjectTypeCount(TrafficObjectInfo toi)
+        {
+            if (!_laneObjectTypeCount.ContainsKey(toi.LaneIndex))
+            {
+                _laneObjectTypeCount.TryAdd(toi.LaneIndex, new ConcurrentDictionary<string, int>());
+            }
+
+            _laneObjectTypeCount.TryGetValue(toi.LaneIndex, out var typesCountsByLaneId);
+            if (!typesCountsByLaneId.ContainsKey(toi.Type))
+            {
+                typesCountsByLaneId.TryAdd(toi.Type, 0);
+            }
+
+            typesCountsByLaneId.TryGetValue(toi.Type, out var typeCounts);
+            typeCounts++;
+            typesCountsByLaneId.TryUpdate(toi.Type, typeCounts, typeCounts - 1);
         }
 
         public int GetLaneObjectTypeCount(int laneId, string typeName)
         {
-            if (!_laneObjectTypeCounts.ContainsKey(laneId))
+            if (!_laneObjectTypeCount.ContainsKey(laneId))
             {
                 return 0;
             }
 
-            _laneObjectTypeCounts.TryGetValue(laneId, out var typesCountsByLaneId);
+            _laneObjectTypeCount.TryGetValue(laneId, out var typesCountsByLaneId);
             if (!typesCountsByLaneId.ContainsKey(typeName))
             {
                 return 0;
@@ -179,17 +200,7 @@ namespace Overlord.Domain.Services
             return typeCounts;
         }
 
-        public void OnCompleted()
-        {
-            // Do nothing
-        }
-
-        public void OnError(Exception error)
-        {
-            // Do nothing
-        }
-
-        public void OnNext(ObjectExpiredEvent value)
+        public override void OnNext(ObjectExpiredEvent value)
         {
             Task.Run(() =>
             {
